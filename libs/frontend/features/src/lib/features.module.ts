b3d273055779fc4b7a7db57823a66fa3c0b2c2ee
Robin Schellius <r.schellius@avans.nl>
2024-11-13 13:40:32 +0100

    Updated

---
+++ libs/frontend/features/src/lib/features.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserDetailsComponent } from './users/user-details/user-details.component';
+import { UserListComponent } from './users/user-list/user-list.component';
+import { UserEditComponent } from './users/user-edit/user-edit.component';
+
+@NgModule({
+    imports: [CommonModule],
+    declarations: [
+        UserDetailsComponent,
+        UserDetailsComponent,
+        UserListComponent,
+        UserEditComponent
+    ]
+})
+export class FeaturesModule {}
--- libs/share-a-meal/common/src/lib/entity/entity.model.ts
+++ libs/share-a-meal/common/src/lib/entity/entity.model.ts
@@ -1,8 +1,8 @@
 /**
  * Base class for all entities that are part of communication to/from services.
  */
-import { Id } from '@avans-nx-workshop/shared/api';
+// import { Id } from '@avans-nx-workshop/shared/api';
 
 export interface IEntity {
-    readonly _id: Id;
+    readonly _id: string; //Id;
 }
