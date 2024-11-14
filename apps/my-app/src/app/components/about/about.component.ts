f05f652bbf467d28e123006b28d1a30961537959
Robin Schellius <r.schellius@avans.nl>
2024-11-14 14:33:11 +0100

    Update voor release

--- apps/my-app/src/app/components/about/about.component.ts
+++ apps/my-app/src/app/components/about/about.component.ts
@@ -3,6 +3,5 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'avans-nx-workshop-about',
     templateUrl: './about.component.html',
-    styleUrls: ['./about.component.css']
 })
 export class AboutComponent {}
--- apps/my-app/src/app/components/dashboard/dashboard.component.ts
+++ apps/my-app/src/app/components/dashboard/dashboard.component.ts
@@ -3,6 +3,5 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'avans-nx-workshop-dashboard',
     templateUrl: './dashboard.component.html',
-    styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {}
