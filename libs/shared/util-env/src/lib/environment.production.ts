8cd31adc67b29056478331c032a23f5809804060
JoepieSnels <j.snels@student.avans.nl>
2024-11-28 22:07:37 +0100

    new api host

--- libs/shared/util-env/src/lib/environment.production.ts
+++ libs/shared/util-env/src/lib/environment.production.ts
@@ -6,8 +6,7 @@ export const environment: IEnvironment = {
     ROOT_DOMAIN_URL: 'https://polite-island-02ad37803.5.azurestaticapps.net',
 
     // Full backend API URL
-    dataApiUrl:
-        'https://api-to-do-ghf4ducsanarb4cf.westeurope-01.azurewebsites.net',
+    dataApiUrl: 'https://to-do-api.azurewebsites.net',
 
     // MongoDB connection string (keep this sensitive data secure)
     MONGO_DB_CONNECTION_STRING:
