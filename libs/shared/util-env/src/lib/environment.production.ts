6890322dd254ec2837c070431e258eefcae8f69d
JoepieSnels <j.snels@student.avans.nl>
2024-11-28 15:14:37 +0100

    Update environment.production.ts

--- libs/shared/util-env/src/lib/environment.production.ts
+++ libs/shared/util-env/src/lib/environment.production.ts
@@ -4,8 +4,12 @@ export const environment: IEnvironment = {
     production: true,
 
     ROOT_DOMAIN_URL: 'https://orange-moss-087212903.5.azurestaticapps.net',
-    dataApiUrl: 'api-to-do-ghf4ducsanarb4cf.westeurope-01.azurewebsites.net',
 
+    // Full backend API URL
+    dataApiUrl:
+        'https://api-to-do-ghf4ducsanarb4cf.westeurope-01.azurewebsites.net',
+
+    // MongoDB connection string (keep this sensitive data secure)
     MONGO_DB_CONNECTION_STRING:
         'mongodb+srv://jsnels:cg2mh2E25GQ3Qn9@to-do.pxd9m.mongodb.net/'
 };
