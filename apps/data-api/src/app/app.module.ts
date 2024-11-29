3cf78692be91325cbf7a42bcb28ec81e3379d32c
JoepieSnels <j.snels@student.avans.nl>
2024-11-29 10:47:57 +0100

    Update app.module.ts

--- apps/data-api/src/app/app.module.ts
+++ apps/data-api/src/app/app.module.ts
@@ -7,17 +7,17 @@ import { environment } from '@avans-nx-workshop/shared/util-env';
 import { Logger } from '@nestjs/common';
 
 Logger.verbose(
-    `Mongoose db connection string: ${process.env.MONGO_DB_CONNECTION_STRING}`
+    `Mongoose db connection string: ${environment.MONGO_DB_CONNECTION_STRING}`
 );
 @Module({
     imports: [
         AuthModule,
-        MongooseModule.forRoot(process.env.MONGO_DB_CONNECTION_STRING, {
+        MongooseModule.forRoot(environment.MONGO_DB_CONNECTION_STRING, {
             connectionFactory: (connection) => {
                 connection.on('connected', () => {
                     // console.log('is connected');
                     Logger.verbose(
-                        `Mongoose db connected to ${process.env.MONGO_DB_CONNECTION_STRING}`
+                        `Mongoose db connected to ${environment.MONGO_DB_CONNECTION_STRING}`
                     );
                 });
                 connection._events.connected();
